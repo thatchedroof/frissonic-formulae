736c227527feaecc76e23a45294906e8999f8dbd
thatchedroof <tnert.wire100@gmail.com>
2025-10-10 04:46:49 -0700

    Update guitar_chords dependency source and path

--- frissonic-formulae/target/wasm32-unknown-unknown/debug/deps/frissonic_formulae.d
+++ frissonic-formulae/target/wasm32-unknown-unknown/debug/deps/frissonic_formulae.d
@@ -1,8 +1,8 @@
-C:\Users\tnert\OneDrive\Documents\GitHub\react-testing\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\frissonic_formulae.d: src\lib.rs src\path.rs
+C:\Users\tnert\OneDrive\Documents\GitHub\frissonic-formulae\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\frissonic_formulae.d: src\lib.rs src\path.rs
 
-C:\Users\tnert\OneDrive\Documents\GitHub\react-testing\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\frissonic_formulae.wasm: src\lib.rs src\path.rs
+C:\Users\tnert\OneDrive\Documents\GitHub\frissonic-formulae\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\frissonic_formulae.wasm: src\lib.rs src\path.rs
 
-C:\Users\tnert\OneDrive\Documents\GitHub\react-testing\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\libfrissonic_formulae.rlib: src\lib.rs src\path.rs
+C:\Users\tnert\OneDrive\Documents\GitHub\frissonic-formulae\frissonic-formulae\target\wasm32-unknown-unknown\debug\deps\libfrissonic_formulae.rlib: src\lib.rs src\path.rs
 
 src\lib.rs:
 src\path.rs:
